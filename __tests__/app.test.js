207371f24761bd4e9333454eaf4026072765b3bc
Jamie <jamiec145@gmail.com>
2025-03-13 16:11:15 +0000

    Challenge_4_articles_comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,18 +1,18 @@
 const endpointsJson = require("../endpoints.json");
 
 const db = require("../db/connection")
-const seed=require('../db/seeds/seed')
+const seed = require('../db/seeds/seed')
 const request = require("supertest");
 const data = require("../db/data/test-data");
 const app = require("../app");
 /* Set up your test imports here */
 
-beforeEach(()=> {
+beforeEach(() => {
 
 
   return seed(data)
 })
-afterAll(()=> {
+afterAll(() => {
   return db.end()
 })
 
@@ -28,49 +28,88 @@ describe("GET /api", () => {
       });
   });
 });
-describe("GET /api/topics", () =>{
-  test("To respond with topics ", () =>{
+describe("GET /api/topics", () => {
+  test("To respond with topics ", () => {
     return request(app)
-    .get("/api/topics")
-    .expect(200)
-    .then(({body: {topics } })=> {
-      expect(topics.length).toBeGreaterThan(0)
-      expect(topics.length).toBe(3)
-      expect(Array.isArray(topics)).toBe(true)
-      topics.forEach((topic) =>{
-        expect(typeof topic.slug).toBe('string')
-        expect(typeof topic.description).toBe('string')
-      
-      }) 
-    })
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBeGreaterThan(0)
+        expect(topics.length).toBe(3)
+        expect(Array.isArray(topics)).toBe(true)
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe('string')
+          expect(typeof topic.description).toBe('string')
+
+        })
+      })
   })
 })
-describe('GET /api/articles/:article_id', () =>{
-  test('Responds with an article object contaiing properties',() =>{
+describe('GET /api/articles/:article_id', () => {
+  test('Responds with an article object contaiing properties', () => {
     return request(app)
-    .get("/api/articles/3")
-    .expect(200)
-    .then(({body: {article}}) =>{
-      expect(article).toBeDefined();
-      expect(article.article_id).toBe(3)
-      expect(article).toHaveProperty('author');
-      expect(article).toHaveProperty('title');
-      expect(article).toHaveProperty('article_id');
-      expect(article).toHaveProperty('body');
-      expect(article).toHaveProperty('topic');
-      expect(article).toHaveProperty('created_at');
-      expect(article).toHaveProperty('votes');
-      expect(article).toHaveProperty('article_img_url');
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toBeDefined();
+        expect(article.article_id).toBe(3)
+        expect(article).toHaveProperty('author');
+        expect(article).toHaveProperty('title');
+        expect(article).toHaveProperty('article_id');
+        expect(article).toHaveProperty('body');
+        expect(article).toHaveProperty('topic');
+        expect(article).toHaveProperty('created_at');
+        expect(article).toHaveProperty('votes');
+        expect(article).toHaveProperty('article_img_url');
 
-    })
-  } )
+      })
+  })
   test('responds with 404 when article is not found', () => {
     return request(app)
-      .get('/api/articles/99') 
+      .get('/api/articles/99')
       .expect(404)
       .then(({ body }) => {
         expect(body.message).toBe('Article not found');
       });
   });
+  test('responds with 400 when article is not valid', () => {
+    return request(app)
+      .get('/api/articles/notAnID')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toBe('Bad Request')
+      })
+  })
 
 })
+describe('Get all articles', () => {
+  describe('GET /api/articles', () => {
+      test('respond with all articles', () => {
+          return request(app)
+              .get('/api/articles')
+              .expect(200)
+              .then(({ body }) => {
+                  expect(body.articles.length).toBeGreaterThan(0); 
+                  expect(Array.isArray(body.articles)).toBe(true); 
+                  body.articles.forEach((article) => {
+                      expect(article).toHaveProperty('author');
+                      expect(article).toHaveProperty('title');
+                      expect(article).toHaveProperty('article_id');
+                      expect(article).toHaveProperty('topic');
+                      expect(article).toHaveProperty('created_at');
+                      expect(article).toHaveProperty('votes');
+                      expect(article).toHaveProperty('article_img_url')
+                      expect(article).toHaveProperty('comment_count');
+                  }); 
+              });
+      });
+      test('for errors', ()=>{
+        return request(app)
+        .get('/api/arrticles')
+        .expect(404)
+        .then(({body}) =>{
+          expect(body.message).toBe('Path does not exist')
+        })
+      })
+  });
+});
